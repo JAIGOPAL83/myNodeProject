6a5fc62f9b97139d22d57b52d54cbfb9e25a6aa1
JAIGOPAL83 <jaigopal.senapati@gmail.com>
2017-11-30 20:51:19 +0800

    First commit by Jai

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+app.set('view engine','hbs');
+app.use(express.static(__dirname+'/public/'));
+
+app.get('/', (req, res)=> {
+    res.send('Hello Express');
+});
+
+app.get('/about', (req, res)=> {
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currYear: new Date().getFullYear()
+    });
+});
+
+app.listen(3000, ()=> {
+    console.log('Server is up on port 3000');
+});
